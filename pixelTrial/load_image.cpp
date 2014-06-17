10f166e6bcf034c76d570363faae68543b7e4513
Adarsh Kosuru <adarsh.kosuru@frogdesign.com>
2014-06-17 13:15:55 -0400

    Load Image class created

---
+++ pixelTrial/load_image.cpp
@@ -0,0 +1,9 @@
+//
+//  load_image.cpp
+//  pixelTrial
+//
+//  Created by Adarsh Kosuru on 6/17/14.
+//  Copyright (c) 2014 Frog. All rights reserved.
+//
+
+#include "load_image.h"
---
+++ pixelTrial/load_image.h
@@ -0,0 +1,14 @@
+//
+//  load_image.h
+//  pixelTrial
+//
+//  Created by Adarsh Kosuru on 6/17/14.
+//  Copyright (c) 2014 Frog. All rights reserved.
+//
+
+#ifndef __pixelTrial__load_image__
+#define __pixelTrial__load_image__
+
+#include <iostream>
+
+#endif /* defined(__pixelTrial__load_image__) */
