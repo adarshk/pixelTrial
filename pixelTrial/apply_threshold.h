83469251b36741c98e9a4154515481494cf2e0fb
Adarsh Kosuru <adarsh.k09@gmail.com>
2014-06-18 14:04:44 -0400

    Added threshold functionality

---
+++ pixelTrial/apply_threshold.h
@@ -0,0 +1,14 @@
+//
+//  apply_threshold.h
+//  pixelTrial
+//
+//  Created by Adarsh Kosuru on 6/18/14.
+//  Copyright (c) 2014 Frog. All rights reserved.
+//
+
+#ifndef __pixelTrial__apply_threshold__
+#define __pixelTrial__apply_threshold__
+
+#include <iostream>
+
+#endif /* defined(__pixelTrial__apply_threshold__) */
